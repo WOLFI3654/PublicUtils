2be266d54852df2184b888d228ad7c4e27d050f4
WOLFI3654 <luigi3654@gmail.com>
2017-08-26 16:59:24 +0200

    added loop feature

--- src/com/xxmicloxx/NoteBlockAPI/SongPlayer.java
+++ src/com/xxmicloxx/NoteBlockAPI/SongPlayer.java
@@ -13,248 +13,269 @@ import org.bukkit.entity.Player;
 
 public abstract class SongPlayer {
 
-    protected Song song;
-    protected boolean playing = false;
-    protected short tick = -1;
-    protected Map<String, Boolean> playerList = Collections.synchronizedMap(new HashMap<String, Boolean>());
-    protected boolean autoDestroy = false;
-    protected boolean destroyed = false;
-    protected Thread playerThread;
-    protected byte fadeTarget = 100;
-    protected byte volume = 100;
-    protected byte fadeStart = volume;
-    protected int fadeDuration = 60;
-    protected int fadeDone = 0;
-    protected FadeType fadeType = FadeType.FADE_LINEAR;
-    private final Lock lock = new ReentrantLock();
-    protected NoteBlockPlayerMain plugin;
-    
-    public SongPlayer(Song song) {
-        this.song = song;
-        plugin = NoteBlockPlayerMain.plugin;
-        start();
-    }
+	protected Song song;
+	protected boolean playing = false;
+	protected short tick = -1;
+	protected Map<String, Boolean> playerList = Collections.synchronizedMap(new HashMap<String, Boolean>());
+	protected boolean autoDestroy = false;
+	protected boolean loop = false;
+	protected boolean destroyed = false;
+	protected Thread playerThread;
+	protected byte fadeTarget = 100;
+	protected byte volume = 100;
+	protected byte fadeStart = volume;
+	protected int fadeDuration = 60;
+	protected int fadeDone = 0;
+	protected FadeType fadeType = FadeType.FADE_LINEAR;
+	private final Lock lock = new ReentrantLock();
+	protected NoteBlockPlayerMain plugin;
 
-    public FadeType getFadeType() {
-        return fadeType;
-    }
+	public SongPlayer(Song song) {
+		this.song = song;
+		plugin = NoteBlockPlayerMain.plugin;
+		start();
+	}
 
-    public void setFadeType(FadeType fadeType) {
-        this.fadeType = fadeType;
-    }
+	public FadeType getFadeType() {
+		return fadeType;
+	}
 
-    public byte getFadeTarget() {
-        return fadeTarget;
-    }
+	public void setFadeType(FadeType fadeType) {
+		this.fadeType = fadeType;
+	}
 
-    public void setFadeTarget(byte fadeTarget) {
-        this.fadeTarget = fadeTarget;
-    }
+	public byte getFadeTarget() {
+		return fadeTarget;
+	}
 
-    public byte getFadeStart() {
-        return fadeStart;
-    }
+	public void setFadeTarget(byte fadeTarget) {
+		this.fadeTarget = fadeTarget;
+	}
 
-    public void setFadeStart(byte fadeStart) {
-        this.fadeStart = fadeStart;
-    }
+	public byte getFadeStart() {
+		return fadeStart;
+	}
 
-    public int getFadeDuration() {
-        return fadeDuration;
-    }
+	public void setFadeStart(byte fadeStart) {
+		this.fadeStart = fadeStart;
+	}
 
-    public void setFadeDuration(int fadeDuration) {
-        this.fadeDuration = fadeDuration;
-    }
+	public int getFadeDuration() {
+		return fadeDuration;
+	}
 
-    public int getFadeDone() {
-        return fadeDone;
-    }
+	public void setFadeDuration(int fadeDuration) {
+		this.fadeDuration = fadeDuration;
+	}
 
-    public void setFadeDone(int fadeDone) {
-        this.fadeDone = fadeDone;
-    }
+	public int getFadeDone() {
+		return fadeDone;
+	}
 
-    protected void calculateFade() {
-        if (fadeDone == fadeDuration) {
-            return; // no fade today
-        }
-        double targetVolume = Interpolator.interpLinear(new double[]{0, fadeStart, fadeDuration, fadeTarget}, fadeDone);
-        setVolume((byte)targetVolume);
-        fadeDone++;
-    }
+	public void setFadeDone(int fadeDone) {
+		this.fadeDone = fadeDone;
+	}
 
-    private void start() {
-    	plugin.doAsync(() -> {
-            while (!destroyed) {
-                long startTime = System.currentTimeMillis();
-                lock.lock();
-                try {
-                	if (destroyed || NoteBlockPlayerMain.plugin.isDisabling()){
-                    	break;
-                    }
-                	
-                    if (playing) {
-                        calculateFade();
-                        tick++;
-                        if (tick > song.getLength()) {
-                            playing = false;
-                            tick = -1;
-                            SongEndEvent event = new SongEndEvent(SongPlayer.this);
-                            plugin.doSync(() -> Bukkit.getPluginManager().callEvent(event));
-                            if (autoDestroy) {
-                                destroy();
-                            }
-                            return;
-                        }
-                        
-                        plugin.doSync(() -> {
-	                        for (String s : playerList.keySet()) {
-	                            Player p = Bukkit.getPlayerExact(s);
-	                            if (p == null) {
-	                                // offline...
-	                                continue;
-	                            }
-	                            playTick(p, tick);
-	                        }
-                        });
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    lock.unlock();
-                }
-                
-                if (destroyed){
-                	break;
-                }
-                
-                long duration = System.currentTimeMillis() - startTime;
-                float delayMillis = song.getDelay() * 50;
-                if (duration < delayMillis) {
-                    try {
-                        Thread.sleep((long) (delayMillis - duration));
-                    } catch (InterruptedException e) {
-                        // do nothing
-                    }
-                }
-            }
-        });
-    }
+	protected void calculateFade() {
+		if (fadeDone == fadeDuration) {
+			return; // no fade today
+		}
+		double targetVolume = Interpolator.interpLinear(new double[] { 0, fadeStart, fadeDuration, fadeTarget },
+				fadeDone);
+		setVolume((byte) targetVolume);
+		fadeDone++;
+	}
 
-    public List<String> getPlayerList() {
-    	List<String> list = new ArrayList<String>();
-    	list.addAll(playerList.keySet());
-        return Collections.unmodifiableList(list);
-    }
+	private void start() {
+		plugin.doAsync(() -> {
+			while (!destroyed) {
+				long startTime = System.currentTimeMillis();
+				lock.lock();
+				try {
+					if (destroyed || NoteBlockPlayerMain.plugin.isDisabling()) {
+						break;
+					}
 
-    public void addPlayer(Player p) {
-       lock.lock();
-       try{
-            if (!playerList.containsKey(p.getName())) {
-                playerList.put(p.getName(), false);
-                ArrayList<SongPlayer> songs = NoteBlockPlayerMain.plugin.playingSongs
-                        .get(p.getName());
-                if (songs == null) {
-                    songs = new ArrayList<SongPlayer>();
-                }
-                songs.add(this);
-                NoteBlockPlayerMain.plugin.playingSongs.put(p.getName(), songs);
-            }
-        } finally {
+					if (playing) {
+						calculateFade();
+						tick++;
+						if (tick > song.getLength()) {
+							if(loop){
+								tick = 0;
+								continue;
+							}
+							playing = false;
+							tick = -1;
+							SongEndEvent event = new SongEndEvent(SongPlayer.this);
+							plugin.doSync(() -> Bukkit.getPluginManager().callEvent(event));
+							if (autoDestroy) {
+								destroy();
+							}
+							return;
+						}
+
+						plugin.doSync(() -> {
+							for (String s : playerList.keySet()) {
+								Player p = Bukkit.getPlayerExact(s);
+								if (p == null) {
+									// offline...
+									continue;
+								}
+								playTick(p, tick);
+							}
+						});
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					lock.unlock();
+				}
+
+				if (destroyed) {
+					break;
+				}
+
+				long duration = System.currentTimeMillis() - startTime;
+				float delayMillis = song.getDelay() * 50;
+				if (duration < delayMillis) {
+					try {
+						Thread.sleep((long) (delayMillis - duration));
+					} catch (InterruptedException e) {
+						// do nothing
+					}
+				}
+			}
+		});
+	}
+
+	public List<String> getPlayerList() {
+		List<String> list = new ArrayList<String>();
+		list.addAll(playerList.keySet());
+		return Collections.unmodifiableList(list);
+	}
+
+	public void addPlayer(Player p) {
+		lock.lock();
+		try {
+			if (!playerList.containsKey(p.getName())) {
+				playerList.put(p.getName(), false);
+				ArrayList<SongPlayer> songs = NoteBlockPlayerMain.plugin.playingSongs.get(p.getName());
+				if (songs == null) {
+					songs = new ArrayList<SongPlayer>();
+				}
+				songs.add(this);
+				NoteBlockPlayerMain.plugin.playingSongs.put(p.getName(), songs);
+			}
+		} finally {
 			lock.unlock();
 		}
-    }
+	}
 
-    
-    
-    public boolean getAutoDestroy() {
-        lock.lock();
-        try {
-            return autoDestroy;
-        } finally {
+	public void setLoop(boolean loop) {
+		lock.lock();
+		try {
+			this.loop = loop;
+		} finally {
 			lock.unlock();
 		}
-    }
+	}
 
-    public void setAutoDestroy(boolean value) {
-    	lock.lock();
-        try {
-            autoDestroy = value;
-        } finally {
+	public boolean isLoop() {
+		lock.lock();
+		try {
+			return loop;
+		} finally {
 			lock.unlock();
 		}
-    }
+	}
 
-    public abstract void playTick(Player p, int tick);
-
-    public void destroy() {
-    	lock.lock();
-        try {
-            SongDestroyingEvent event = new SongDestroyingEvent(this);
-            Bukkit.getPluginManager().callEvent(event);
-            //Bukkit.getScheduler().cancelTask(threadId);
-            if (event.isCancelled()) {
-                return;
-            }
-            destroyed = true;
-            playing = false;
-            setTick((short) -1);
-        } finally {
+	public boolean getAutoDestroy() {
+		lock.lock();
+		try {
+			return autoDestroy;
+		} finally {
 			lock.unlock();
 		}
-    }
+	}
 
-    public boolean isPlaying() {
-        return playing;
-    }
-
-    public void setPlaying(boolean playing) {
-        this.playing = playing;
-        if (!playing) {
-            SongStoppedEvent event = new SongStoppedEvent(this);
-            Bukkit.getPluginManager().callEvent(event);
-        }
-    }
-
-    public short getTick() {
-        return tick;
-    }
-
-    public void setTick(short tick) {
-    	this.tick = tick;
-    }
-
-    public void removePlayer(Player p) {
-    	lock.lock();
-        try {
-            playerList.remove(p.getName());
-            if (NoteBlockPlayerMain.plugin.playingSongs.get(p.getName()) == null) {
-                return;
-            }
-            ArrayList<SongPlayer> songs = new ArrayList<SongPlayer>(
-                    NoteBlockPlayerMain.plugin.playingSongs.get(p.getName()));
-            songs.remove(this);
-            NoteBlockPlayerMain.plugin.playingSongs.put(p.getName(), songs);
-            if (playerList.isEmpty() && autoDestroy) {
-                SongEndEvent event = new SongEndEvent(this);
-                Bukkit.getPluginManager().callEvent(event);
-                destroy();
-            }
-        } finally {
+	public void setAutoDestroy(boolean value) {
+		lock.lock();
+		try {
+			autoDestroy = value;
+		} finally {
 			lock.unlock();
 		}
-    }
+	}
 
-    public byte getVolume() {
-        return volume;
-    }
+	public abstract void playTick(Player p, int tick);
 
-    public void setVolume(byte volume) {
-        this.volume = volume;
-    }
+	public void destroy() {
+		lock.lock();
+		try {
+			SongDestroyingEvent event = new SongDestroyingEvent(this);
+			Bukkit.getPluginManager().callEvent(event);
+			// Bukkit.getScheduler().cancelTask(threadId);
+			if (event.isCancelled()) {
+				return;
+			}
+			destroyed = true;
+			playing = false;
+			setTick((short) -1);
+		} finally {
+			lock.unlock();
+		}
+	}
 
-    public Song getSong() {
-        return song;
-    }
+	public boolean isPlaying() {
+		return playing;
+	}
+
+	public void setPlaying(boolean playing) {
+		this.playing = playing;
+		if (!playing) {
+			SongStoppedEvent event = new SongStoppedEvent(this);
+			Bukkit.getPluginManager().callEvent(event);
+		}
+	}
+
+	public short getTick() {
+		return tick;
+	}
+
+	public void setTick(short tick) {
+		this.tick = tick;
+	}
+
+	public void removePlayer(Player p) {
+		lock.lock();
+		try {
+			playerList.remove(p.getName());
+			if (NoteBlockPlayerMain.plugin.playingSongs.get(p.getName()) == null) {
+				return;
+			}
+			ArrayList<SongPlayer> songs = new ArrayList<SongPlayer>(
+					NoteBlockPlayerMain.plugin.playingSongs.get(p.getName()));
+			songs.remove(this);
+			NoteBlockPlayerMain.plugin.playingSongs.put(p.getName(), songs);
+			if (playerList.isEmpty() && autoDestroy) {
+				SongEndEvent event = new SongEndEvent(this);
+				Bukkit.getPluginManager().callEvent(event);
+				destroy();
+			}
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public byte getVolume() {
+		return volume;
+	}
+
+	public void setVolume(byte volume) {
+		this.volume = volume;
+	}
+
+	public Song getSong() {
+		return song;
+	}
 }
